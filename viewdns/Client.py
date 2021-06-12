446f5ed33f56b0accd18d2c0d0905d4908a97ffc
Michael Scribellito <mscribellito@gmail.com>
2021-06-12 14:40:56 -0400

    Comments

--- viewdns/Client.py
+++ viewdns/Client.py
@@ -11,10 +11,28 @@ class Client():
     api_key = None
 
     def __init__(self, api_key):
+        """
+        Creates a new instance of the ViewDNS.info API client.
+
+        The ViewDNS.info API allows webmasters to integrate the tools provided by ViewDNS.info into their own sites in a simple and effective manner.
+
+        Docs: https://viewdns.info/api/docs/
+        """
 
         self.api_key = api_key
     
     def get_dns_records(self, domain, record_type='ANY'):
+        """
+        View all configured DNS records (A, MX, CNAME etc.) for a specified domain name.
+
+        Params:
+
+        * domain - the domain name to lookup DNS records for
+
+        * recordtype - the type of DNS record you wish to retrieve (default 'ANY')
+
+        Docs: https://viewdns.info/api/docs/dns-record-lookup.php
+        """
 
         params = dict()
         params['domain'] = domain
@@ -25,6 +43,15 @@ class Client():
         return data
     
     def get_ip_location(self, ip):
+        """
+        This tool will display geographic information about a supplied IP address including city, country, latitude, longitude and more.
+        
+        Params:
+
+        * ip - the ip address to find the location of
+
+        Docs: https://viewdns.info/api/docs/ip-location-finder.php
+        """
 
         params = dict()
         params['ip'] = ip
